92df0444fb28d746b981619b0aac1ce1bfdd222a
mich <gateway1@gateway1-PC>
2018-10-31 17:54:31 -0600

    eleccion entre las 2 empresas e impresion de reporte de las mismas

--- src/Persistencia/db.java
+++ src/Persistencia/db.java
@@ -7,13 +7,9 @@ package Persistencia;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
